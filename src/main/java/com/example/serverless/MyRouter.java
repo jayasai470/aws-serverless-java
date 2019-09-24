921c029ec2ac122384637db043abee8e39594787
j.muppala <j.muppala@samsung.com>
2019-09-24 14:01:49 -0500

    samlpe api end point

--- src/main/java/com/example/serverless/MyRouter.java
+++ src/main/java/com/example/serverless/MyRouter.java
@@ -3,9 +3,12 @@ package com.example.serverless;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
@@ -14,6 +17,10 @@ public class MyRouter {
     @Bean
     public RouterFunction<ServerResponse> routes() {
         return RouterFunctions
-                .route(GET("/api/status"), request -> ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).syncBody("status is up"));
+                .route(GET("/api/status"), request -> ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).syncBody("status is up"))
+                .andRoute(GET("/api/time"), request -> ServerResponse.ok().body(BodyInserters.fromObject(SampleResp.builder().localDateTime(LocalDateTime.now()).status("up").build())))
+                ;
+
     }
 }
+
